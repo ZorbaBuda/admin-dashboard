76f6e313a563a68633fc94c65e2cfdb11651ad73
Fran <fm-a@robindatahood.com>
2024-08-09 19:39:07 +0200

    first commit

---
+++ types/index.d.ts
@@ -0,0 +1,61 @@
+export enum FormFieldType {
+  INPUT = "input",
+  TEXTAREA = "textarea",
+  PHONE_INPUT = "phoneInput",
+  CHECKBOX = "checkbox",
+  DATE_PICKER = "datePicker",
+  SELECT = "select",
+  SKELETON = "skeleton",
+}
+
+type SearchParams = {
+  page: string;
+  limit: string;
+  sort: string;
+  search: string;
+};
+
+type FeaturedImage = {
+  imageId: string;
+  imageUrl: string;
+  imageTitle: string;
+  altText: string;
+};
+
+type TBlog = {
+  id: string;
+  title: string;
+  slug: string;
+  body: string;
+  author?: string;
+  metaDescription: string;
+  categories: string[];
+  published: boolean;
+  bodyImages?: string[];
+  featuredImage: FeaturedImage;
+  userId: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+type TCategory = {
+  id: string,
+  categoryName: string,
+  slug: string,
+  description: string,
+  blogCount?: string,
+  userId?: string,
+  createdAt: string,
+  updatedAt: string,
+}
+
+type TMedia = {
+  id: string,
+  imageId : string,
+  imageUrl : string,
+  imageTitle : string,
+  altText : string,
+  userId: string,
+  createdAt: string,
+  updatedAt: string,
+}
